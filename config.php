bf47d14c3974f148d5159377b954547e216dac0d
yg lee <yglee@ygs-MacBook-Air.local>
2019-06-17 13:45:49 -0700

    set env variable for prod

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 
 <?php
-
-$google_client_id = "448788181225-5vemsffjm1oecp880fddhvbgmv2fj9r4.apps.googleusercontent.com";
-$google_client_secret = "q-BRBuKp5vZJQrfyN2s9CNiv";
+    if(getenv('GOOGLE_CLIENT_ID') !== false){
+        $google_client_id = getenv('GOOGLE_CLIENT_ID');
+        $google_client_secret = getenv('GOOGLE_CLIENT_SECRET');
+    }    
+    else{
+        $google_client_id = "448788181225-5vemsffjm1oecp880fddhvbgmv2fj9r4.apps.googleusercontent.com";
+        $google_client_secret = "q-BRBuKp5vZJQrfyN2s9CNiv";
+    }    
+    
 ?>
